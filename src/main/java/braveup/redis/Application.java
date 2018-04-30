b79e1ab0f6981b3aa1f415509998c5c1b8006e50
lei.song <lei.song@leisongdeMacBook-Pro.local>
2018-05-01 01:06:05 +0800

    先分类两个要开始学习的地方，后面在增加

---
+++ src/main/java/braveup/redis/Application.java
@@ -0,0 +1,13 @@
+package braveup.redis;
+
+/**
+ * braveup-lei.song
+ *
+ * @author
+ * @create 2018-05-01 上午1:01
+ **/
+public class Application {
+    public static void main(String[] args) {
+        // TODO: 2018/5/1  开始reids的系统学习
+    }
+}
---
+++ src/main/java/braveup/zk/Application.java
@@ -0,0 +1,13 @@
+package braveup.zk;
+
+/**
+ * braveup-lei.song
+ *
+ * @author
+ * @create 2018-05-01 上午1:00
+ **/
+public class Application {
+    public static void main(String[] args) {
+        // TODO: 2018/5/1 开始zk的系统学习
+    }
+}
